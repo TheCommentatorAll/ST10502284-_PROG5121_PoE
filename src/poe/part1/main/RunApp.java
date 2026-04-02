9373fa654c774d5d87c60c4ff5b61483856fa5a2
Nicholas Morris <st10502284@rcconnect.edu.za>
2026-04-02 18:45:38 +0200

    Part 1 | Created Loopable registration process
    Used a do-while loop with a boolean argument to Allow for repeats of the registration process incase of incorrectly formatted user input

--- src/poe/part1/main/RunApp.java
+++ src/poe/part1/main/RunApp.java
@@ -10,55 +10,71 @@ import java.util.Scanner;
 import poe.part1.logic.Login;
 
 public class RunApp {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         //create a login object called auth
         Login auth = new Login();
+        //while loop condition
+        boolean registrationStatus = false;
+
+        do {//start of do-while
+            //--- REGISTRATION PHASE ---
+            System.out.println("-- REGISTER NEW ACCOUNT --");
+            System.out.print("Enter your Name: ");
+            String registerName = input.nextLine();
+
+            System.out.print(">" + "\nEnter your surname: ");
+            String registerSurname = input.nextLine();
+
+            System.out.print(">" + "\nEnter your Username: ");
+            String registerUsername = input.nextLine();
+
+            System.out.print(">" + "\nEnter your Password: ");
+            String registerPassword = input.nextLine();
+
+            System.out.print(">" + "\nEnter your Cellphone Number: ");
+            String registerNumber = input.nextLine();
+            System.out.println("-----------------------------------");
+
+            String regStatus = auth.registerUser(registerUsername, registerPassword, registerName, registerSurname, registerNumber);
+
+            //Only carry onto User login if registration process was a success
+            if (regStatus.contains("registered successfully")) {
+
+                System.out.println(regStatus);
+                System.out.println("-----------------------------------");
+                System.out.println("-- LOGIN DETAILS --");
+                System.out.println("Your username is: " + registerUsername + "\nYour password is: " + registerPassword);
+                System.out.println("-----------------------------------");
+
+                //--- USER LOGIN PHASE ---
+                System.out.println("-- LOGIN TO ACCOUNT --");
+                System.out.print("Please enter your Username: ");
+                String username = input.nextLine();
+
+                System.out.print(">" + "\nPlease enter your Password: ");
+                String password = input.nextLine();
+                System.out.println("-----------------------------------");
+
+                boolean isSuccess = auth.loginUser(username, password);
+
+                String finalMessage = auth.returnLoginStatus(isSuccess);
+                System.out.println("-- STATUS --");
+                System.out.println(finalMessage);
+
+                registrationStatus = true;
+
+            }else {
+                System.out.println("\n[ERROR] \n" + regStatus);
+                System.out.println("Please try registering again.");
                 
-        //--- REGISTRATION PHASE ---
-        System.out.println("-- REGISTER NEW ACCOUNT --");       
-        System.out.print("Enter your Name: ");
-        String registerName = input.nextLine();
+            }
+
+        } while (!registrationStatus);//end of do-while
         
-        System.out.print(">" + "\nEnter your surname: ");
-        String registerSurname = input.nextLine();
-        
-        System.out.print(">" +"\nEnter your Username: ");
-        String registerUsername = input.nextLine();
-        
-        System.out.print(">" +"\nEnter your Password: ");
-        String registerPassword = input.nextLine();
-        
-        System.out.print(">" +"\nEnter your Cellphone Number: " );
-        String registerNumber = input.nextLine();
-        System.out.println("-----------------------------------");
-        
-        String regStatus = auth.registerUser(registerUsername, registerPassword, registerName, registerSurname, registerNumber);
-        System.out.println(regStatus);
-        
-        System.out.println("-----------------------------------");
-        System.out.println("-- LOGIN DETAILS --");
-        System.out.println("Your username is: " + registerUsername + "\nYour password is: " + registerPassword);
-        System.out.println("-----------------------------------");
-        
-        //Only carry onto User login if registration process was a success
-        if(regStatus.contains("registered successfully")){
-        
-        //--- USER LOGIN PHASE ---
-        System.out.println("-- LOGIN TO ACCOUNT --");
-        System.out.print("Please enter your Username: ");
-        String username = input.nextLine();
-        
-        System.out.print(">" +"\nPlease enter your Password: ");
-        String password = input.nextLine();
-        System.out.println("-----------------------------------");
-        
-        boolean isSuccess = auth.loginUser(username, password);
-        
-        String finalMessage = auth.returnLoginStatus(isSuccess);
-        System.out.println("-- STATUS --");
-        System.out.println(finalMessage);
-    }
         input.close();
- }
+
+    }
+
 }
