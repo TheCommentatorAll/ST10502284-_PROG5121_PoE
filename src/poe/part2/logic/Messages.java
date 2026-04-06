3a2bad06038830728edf15cbc457d88f46c9d2de
Nicholas Morris <st10502284@rcconnect.edu.za>
2026-04-06 14:08:12 +0200

    Part 2 | Project Layout
    minor change to project layout

---
+++ src/poe/part2/logic/Messages.java
@@ -0,0 +1,19 @@
+/*
+ * 
+ *-------------------------------------------------------------------------
+ * File: [FileName].java
+ * Developer: [Your Name]
+ * Student ID: [Your Number]
+ * Date: March 26, 2026
+ * * Description: 
+ * -------------------------------------------------------------------------
+ */
+package poe.part2.logic;
+
+/**
+ *
+ * @author nexis
+ */
+public class Messages {
+    
+}
